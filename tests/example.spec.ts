8d390247a2b0a48433f024c6a5b63059fabcc34f
unknown <alex.veselinov@ten10.com>
2024-07-15 14:28:06 +0100

    matt update

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -26,13 +26,16 @@ test('product desc checker ',async ({ page }) => {
   await expect(page.getByText('The Samsung Galaxy S6 is')).toBeVisible();
 })
 
-//the tests below wont work on firefox, I made the first test by hand and the other with codegen on firefox but it just doesnt add the item to the cart when I look at in UI mode
+//the tests below wont work on firefox, I made the first test by nphand and the other with codegen on firefox but it just doesnt add the item to the cart when I look at in UI mode
 
 
 test('adding to cart', async ({ page }) =>{
   await page.goto('https://www.demoblaze.com/')
   await page.getByRole('link',{ name: 'Nokia lumia 1520'}).click();
+  await expect(page.getByRole('link', { name: 'Add to cart', exact: true })).toBeVisible();
   await page.getByRole('link', { name: 'Add to cart '}).click();
+  await page.waitForResponse(res => res.status() === 200) // this waits for a response code 200 code (which is the request of pressing add to cart button)
+  await page.waitForLoadState('networkidle'); // this waits for there to be no traffic in the network (press f12 and go look at network tab)
   await page.getByRole('link', { name: 'Cart', exact: true }).click();
   await expect(page.getByRole('cell', { name: 'Nokia lumia' })).toBeVisible();
 } )
